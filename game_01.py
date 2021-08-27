e44d46f4eb9a358bddca6d79c33df4031acef9c4
pash10 <66901816+pash10@users.noreply.github.com>
2021-08-28 00:36:40 +0300

    Update game_01.py

--- game_01.py
+++ game_01.py
@@ -1,11 +1,8 @@
-
-
 theBored = {
     "7": "", "8" : "", "9":"",
     "4": "","5" : "","6":"",
     "1":"","2":"", "3":""
 }
-
 
 def printBord():
     print(theBored["7"] + " |" + theBored["8"] + "  | " + theBored["9"])
